75239c9a5b22c46f7880c6f6e4aaf825375b0037
Karim Rhoualem <karim.rhoualem@gmail.com>
2021-07-27 19:14:54 -0400

    Initial commit.

---
+++ C19VS/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C19VS.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Name { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            Name = "Gabbi";
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ C19VS/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace C19VS
+{
+    public class Person
+    {
+        public string _firstName { get; set; }
+        public string _lastName { get; set; }
+        public int _age { get; set; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _age = age;
+        }
+    }
+} 
+	
