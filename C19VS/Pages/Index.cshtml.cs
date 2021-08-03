e08cd6c7a8a3c0915ff099300a6750dcdea07ddf
Karim Rhoualem <karim.rhoualem@gmail.com>
2021-08-03 19:33:55 -0400

    Fixed the index file structure so that the home page displays correctly.

--- C19VS/Pages/Index.cshtml.cs
+++ C19VS/Pages/Index.cshtml.cs
@@ -1,33 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace C19VS.Pages
 {
     public class IndexModel : PageModel
     {
-        public string TABLE_NAME = "Person";
-        public List<object[]> tableList;
-        
-        private DatabaseHelper DatabaseHelper;
-
-        public IndexModel(IDatabaseHelper databaseHelper)
+        public IActionResult OnGet()
         {
-            DatabaseHelper = (DatabaseHelper)databaseHelper;
-        }
-
-        public async Task OnGetAsync()
-        {
-            DatabaseHelper.ConnectDatabase();
-
-            tableList = await DatabaseHelper.SelectAllRecords(TABLE_NAME);
-
-            DatabaseHelper.DisconnectDatabase();
+            return RedirectToPage("/Person/Index");
         }
     }
 }
