7d6990f9b97474bbdb15e53d4ff6a2be2e89c0d1
Ahmed Ali <ahmed.ali@mail.concordia.ca>
2021-08-10 22:25:05 -0400

    * VaccinationFacilityDelete.cshtml: * VaccinationFacilityDelete.cshtml.cs: Created VaccinationFacility   Delete Model
    * VaccinationFacilityEdit.cshtml:
    * VaccinationFacilityEdit.cshtml.cs: Created VaccinationFacility Edit
      Model
    
    * VaccinationFacilityIndex.cshtml:
    * VaccinationFacilityIndex.cshtml.cs: Created VaccinationFacility
      Index Model
    
    * VaccinationFacilityInsert.cshtml:
    * VaccinationFacilityInsert.cshtml.cs: Created VaccinationFacility
      Insert Model
    
    * C19VS.csproj: Added VaccinationFacility Page to page list
    
    * DatabaseHelper.cs: No modifications done
    
    * _Layout.cshtml: Added VaccinationFacility to Menu Bar
    
    * DoseEdit.cshtml: Edited redirect to index from Manager to Dose index

---
+++ C19VS/Pages/VaccinationFacility/VaccinationFacilityInsert.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace C19VS.Pages.VaccinationFacility
+{
+    public class VaccinationFacilityInsertModel : PageModel
+    {
+        public const string TASK_NAME = "VaccinationFacility - VaccinationFacilityInsertModel";
+
+        public Models.VaccinationFacility VaccinationFacility { get; private set; }
+        public DatabaseHelper DatabaseHelper { get; private set; }
+
+        public VaccinationFacilityInsertModel(IDatabaseHelper databaseHelper)
+        {
+            DatabaseHelper = (DatabaseHelper)databaseHelper;
+        }
+
+        public IActionResult OnPostAsync(Models.VaccinationFacility VaccinationFacility)
+        {
+            DatabaseHelper.ConnectDatabase();
+
+            bool insertSuccessful =
+                DatabaseHelper.InsertRecord(
+                    typeof(Models.VaccinationFacility), VaccinationFacility);
+
+            if (insertSuccessful)
+            {
+                Console.WriteLine($"[{TASK_NAME}] " +
+                    $"Vaccination Facility " +
+                    $"{VaccinationFacility.facilityName} " +
+                    $"of {VaccinationFacility.facilityID} " +
+                    $"inserted successfully.");
+            }
+            else
+            {
+                Console.WriteLine($"[{TASK_NAME}] " +
+                    $"Error inserting " +
+                    $"Vaccination Facility " +
+                    $"{VaccinationFacility.facilityName} " +
+                    $"of {VaccinationFacility.facilityID} " +
+                    $"inserted successfully.");
+            }
+
+            DatabaseHelper.DisconnectDatabase();
+
+            return RedirectToPage("/VaccinationFacility/VaccinationFacilityIndex");
+        }
+    }
+}
